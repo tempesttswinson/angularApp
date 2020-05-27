31dad5e1b9912c78c8707843850b5dbf4a3361e0
tempesttswinson <tempestt.swinson@gmail.com>
2020-05-26 23:22:02 -0400

    Angular app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompTwoComponent } from './comp-two/comp-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompTwoComponent
   ],
   imports: [
     BrowserModule,
